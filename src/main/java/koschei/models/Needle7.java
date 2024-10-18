2f7d43e3f3f48aeff6c3fb1b326d95171785504e
pmsmirnov <pmsmirnov@mail.ru>
2024-10-18 15:16:50 +0300

    done

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,24 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private Deth8 deth;
+
+    public Needle7(@Autowired Deth8 dth) {
+        this.deth = dth;
+    }
+
+//    @Autowired
+//    public Needle7( Deth8 dth) {     или так
+//        this.deth = dth;
+//    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth.toString();
     }
 }
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
 
+    Duck5 d;
+
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + d.toString();
+    }
+
+    @Autowired
+    public void setD(Duck5 d) {
+        this.d = d;
     }
 }
